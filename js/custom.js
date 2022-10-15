6afb1c99ead785310f9ece67736fb48eeef0cf73
Dima$ <110098172+Dmytro-Siechnyi@users.noreply.github.com>
2022-10-15 20:54:28 +0300

    дз9 моя часть

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    margin: 200,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    animateIn: "fadeIn",
+    animateOut: "fadeOut",
+  });
+});
